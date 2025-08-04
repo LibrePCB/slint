4a08d456d8b34f12478eb90f93955561221e2b24
U. Bruhin <urbibruhin@bluewin.ch>
2025-08-04 20:26:23 +0200

    Implement hacky translation callback for LibrePCB

--- api/rs/slint/lib.rs
+++ api/rs/slint/lib.rs
@@ -230,7 +230,9 @@ pub use i_slint_core::model::{
 };
 pub use i_slint_core::sharedvector::SharedVector;
 pub use i_slint_core::timers::{Timer, TimerMode};
-pub use i_slint_core::translations::{select_bundled_translation, SelectBundledTranslationError};
+pub use i_slint_core::translations::{
+    select_bundled_translation, set_translate_callback, SelectBundledTranslationError,
+};
 pub use i_slint_core::{
     format,
     string::{SharedString, ToSharedString},
